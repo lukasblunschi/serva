0d559938c6943eaba774d8d5591a8a6b0a53da5b
Lukas Blunschi <sf@lukasblunschi.ch>
2010-05-09 15:28:01 +0000

    releasing 0.2.3.

--- src/ch/serva/ServaConstants.java
+++ src/ch/serva/ServaConstants.java
@@ -10,7 +10,7 @@ public abstract class ServaConstants {
 
 	// ----------------------------------------------------------------- values
 
-	public static final String VERSION = "0.2.2";
+	public static final String VERSION = "0.2.3";
 
 	public static final String NEW = "new";
 
