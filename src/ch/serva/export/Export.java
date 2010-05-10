d8d40ca15db76a3e530d79bbdd519fa127b18df1
Lukas Blunschi <sf@lukasblunschi.ch>
2010-05-10 15:52:19 +0000

    export package added.

---
+++ src/ch/serva/export/Export.java
@@ -0,0 +1,16 @@
+package ch.serva.export;
+
+/**
+ * Interface for all possible exports.
+ * 
+ * @author Lukas Blunschi
+ * 
+ */
+public interface Export {
+
+	/**
+	 * Parameter name for defining the name of the export.
+	 */
+	public static final String P_NAME = "exportName";
+
+}
