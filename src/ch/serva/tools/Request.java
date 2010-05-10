156e94e0d936112d11685774ebd7d6ce3836f67c
Lukas Blunschi <sf@lukasblunschi.ch>
2010-05-10 17:47:04 +0000

    request interface added.

---
+++ src/ch/serva/tools/Request.java
@@ -0,0 +1,13 @@
+package ch.serva.tools;
+
+/**
+ * Basic interface for all requests.
+ * 
+ * @author Lukas Blunschi
+ * 
+ */
+public interface Request {
+
+	String getParameter(String name);
+
+}
