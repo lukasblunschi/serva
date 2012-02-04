8a31031d95c3c16b37c9b6053d80c64f79fafa93
Lukas Blunschi <sf@lukasblunschi.ch>
2012-02-04 16:23:00 +0000

    bookings adder removed from list bookings page. list bookings page overhauled.

--- src/ch/serva/pages/list/ListBookingsPage.java
+++ src/ch/serva/pages/list/ListBookingsPage.java
@@ -1,26 +1,20 @@
 package ch.serva.pages.list;
 
-import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 
-import ch.serva.actions.RemoveBookingAction;
 import ch.serva.config.Config;
-import ch.serva.db.Booking;
 import ch.serva.db.Domain;
 import ch.serva.db.Service;
-import ch.serva.db.collections.Services;
 import ch.serva.localization.Dictionary;
 import ch.serva.pages.AbstractAdminPage;
-import ch.serva.pages.edit.EditBookingPage;
-import ch.serva.pages.elements.adders.BookingsAdder;
 import ch.serva.pages.elements.common.Clearer;
+import ch.serva.pages.elements.lists.DomainBookingsList;
+import ch.serva.pages.elements.lists.ServiceBookingsList;
 import ch.serva.pages.elements.selectors.DomainsSelector;
-import ch.serva.tools.Dates;
-import ch.serva.tools.Escape;
-import ch.serva.tools.comparators.BookingComparator;
+import ch.serva.pages.elements.selectors.ServicesSelector;
 
 /**
  * A page which lists all bookings.
@@ -42,75 +36,33 @@ public class ListBookingsPage extends AbstractAdminPage {
 		html.append("<!-- title -->\n");
 		html.append("<h3 class='content'>").append(dict.bookings()).append("</h3>\n\n");
 
-		// TODO to work with booking, you can either select a domain OR a service to start with...
+		// domains selector
+		List<Domain> domainsSel = DomainsSelector.getSelectedDomains(req, em, dict, NAME, html);
 
-		// domains
-		List<Domain> domainsSel = DomainsSelector.getSelectedDomains(req, em, dict, ListBookingsPage.NAME, html);
-
-		// multiple service adder
-		if (domainsSel.size() == 1) {
-			Domain domain = domainsSel.get(0);
-			List<Service> servicesNotActive = Services.getNotActive(domain, em);
-			String pagelink = "page=" + NAME + "&amp;" + DomainsSelector.P_DOMAIN_IDS + "=" + domain.getId();
-			new BookingsAdder(domain, servicesNotActive, pagelink).appendHtml(html, config, dict);
-		}
+		// services selector
+		List<Service> servicesSel = ServicesSelector.getSelectedServices(req, em, dict, NAME, html);
 
 		// clearer
 		new Clearer().appendHtml(html, config, dict);
 
-		// action prefixes
-		String pActionRemove = "action=" + RemoveBookingAction.NAME + "&amp;";
-		String pPage = "page=" + ListBookingsPage.NAME + "&amp;";
+		// pagelink
+		String pPage = "page=" + NAME + "&amp;";
 		String pDomainIds = DomainsSelector.getParameterStr(req) + "&amp;";
-		String pBookingIdPrefLong = "booking_" + Booking.F_ID + "=";
-		String removePrefix = "?" + pActionRemove + pPage + pDomainIds + pBookingIdPrefLong;
+		String pagelink = pPage + pDomainIds;
 
 		// bookings
-		html.append("<!-- bookings -->\n");
-		html.append("<table class='content'>\n");
-		html.append("<tr>");
-		html.append("<td>").append(dict.domain()).append("</td>");
-		html.append("<td>").append(dict.service()).append("</td>");
-		html.append("<td>").append(dict.from()).append("</td>");
-		html.append("<td>").append(dict.to()).append("</td>");
-		html.append("<td>").append(dict.info()).append("</td>");
-		html.append("<td>").append(dict.actions()).append("</td>");
-		html.append("</tr>\n");
-		for (Domain domain : domainsSel) {
-			List<Booking> bookings = domain.getBookings();
-			Collections.sort(bookings, new BookingComparator());
-
-			// TODO use DomainBookingsList here!
-
-			html.append("<tr><td colspan='6'>&#160;</td></tr>\n");
-			for (Booking booking : bookings) {
-				html.append("<tr>");
-
-				// domain and service
-				html.append("<td>").append(Escape.safeXml(booking.getDomain().toShortString())).append("</td>");
-				html.append("<td>").append(Escape.safeXml(booking.getService().toShortString())).append("</td>");
-
-				// from, to and info
-				String fromStr = Dates.dateFormat.format(booking.getFrom());
-				String toStr = booking.getTo() == null ? "" : Dates.dateFormat.format(booking.getTo());
-				html.append("<td>").append(fromStr).append("</td>");
-				html.append("<td>").append(toStr).append("</td>");
-				html.append("<td>").append(Escape.safeXml(booking.getInfo())).append("</td>");
-
-				// actions
-				html.append("<td>");
-				html.append("<a href='?page=" + EditBookingPage.NAME + "&amp;id=" + booking.getId() + "'>");
-				html.append(dict.edit());
-				html.append("</a>");
-				if (booking.isRemovable()) {
-					html.append(" <a href='" + removePrefix + booking.getId() + "'>" + dict.remove() + "</a>");
-				}
-				html.append("</td>");
-
-				html.append("</tr>\n");
+		// (from domains or services)
+		if (domainsSel.size() > 0) {
+			for (Domain domain : domainsSel) {
+				String title = domain.toShortString();
+				new DomainBookingsList(domain, title, pagelink).appendHtml(html, config, dict);
+			}
+		} else {
+			for (Service service : servicesSel) {
+				String title = service.toShortString();
+				new ServiceBookingsList(service, title, pagelink).appendHtml(html, config, dict);
 			}
 		}
-		html.append("</table>\n\n");
 
 		return html.toString();
 	}
