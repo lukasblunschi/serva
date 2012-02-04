2a773ddbf0e922062e3e15abd3756f8fbbdd24e1
Lukas Blunschi <sf@lukasblunschi.ch>
2012-02-04 15:22:54 +0000

    pagelink added to domain bookings list element. layout of domain bookings list element changed. actions added to domain bookings list element.

--- src/ch/serva/pages/elements/lists/DomainBookingsList.java
+++ src/ch/serva/pages/elements/lists/DomainBookingsList.java
@@ -3,6 +3,7 @@ package ch.serva.pages.elements.lists;
 import java.util.Collections;
 import java.util.List;
 
+import ch.serva.actions.RemoveBookingAction;
 import ch.serva.config.Config;
 import ch.serva.db.Booking;
 import ch.serva.db.Domain;
@@ -25,8 +26,11 @@ public class DomainBookingsList implements Element {
 
 	private final Domain domain;
 
-	public DomainBookingsList(Domain domain) {
+	private final String pagelink;
+
+	public DomainBookingsList(Domain domain, String pagelink) {
 		this.domain = domain;
+		this.pagelink = pagelink;
 	}
 
 	public void appendHtml(StringBuffer html, Config config, Dictionary dict) {
@@ -37,31 +41,39 @@ public class DomainBookingsList implements Element {
 
 		// element
 		html.append("<!-- domain bookings list -->\n");
-		html.append("<div class='content floatleft'>\n");
+		html.append("<div class='content'>\n");
 
 		// element title
 		html.append("<h4>").append(dict.bookings()).append("</h4>\n");
 
+		// action prefixes
+		String pActionRemove = "action=" + RemoveBookingAction.NAME + "&amp;";
+		String pBookingIdPrefLong = "booking_" + Booking.F_ID + "=";
+		String removePrefix = "?" + pActionRemove + pagelink + "&amp;" + pBookingIdPrefLong;
+
 		// table
 		html.append("<table>\n");
 		html.append("<tr>");
+		html.append("<td>").append(dict.domain()).append("</td>");
 		html.append("<td>").append(dict.service()).append("</td>");
+		html.append("<td>").append(dict.price()).append("</td>");
 		html.append("<td>").append(dict.from()).append("</td>");
 		html.append("<td>").append(dict.to()).append("</td>");
 		html.append("<td>").append(dict.info()).append("</td>");
-		html.append("<td>").append(dict.price()).append("</td>");
 		html.append("<td>").append(dict.payments()).append("</td>");
+		html.append("<td>").append(dict.actions()).append("</td>");
 		html.append("</tr>\n");
 		for (Booking booking : bookings) {
 			html.append("<tr>");
 
-			// service
+			// domain and service
+			Domain domain = booking.getDomain();
 			Service service = booking.getService();
-			html.append("<td>");
-			html.append("<a href='?page=" + EditBookingPage.NAME + "&amp;id=" + booking.getId() + "'>");
-			html.append(Escape.safeXml(service.toShortString()));
-			html.append("</a>");
-			html.append("</td>");
+
+			// domain, service and price
+			html.append("<td>").append(Escape.safeXml(domain.toShortString())).append("</td>");
+			html.append("<td>").append(Escape.safeXml(service.toShortString())).append("</td>");
+			html.append("<td>").append(Doubles.formatter.format(service.getPrice())).append("</td>");
 
 			// from, to and info
 			String fromStr = Dates.dateFormat.format(booking.getFrom());
@@ -70,10 +82,19 @@ public class DomainBookingsList implements Element {
 			html.append("<td>").append(toStr).append("</td>");
 			html.append("<td>").append(Escape.safeXml(booking.getInfo())).append("</td>");
 
-			// price and payments
+			// payments
 			boolean hasPayments = booking.getPayments().size() > 0;
-			html.append("<td>").append(Doubles.formatter.format(service.getPrice())).append("</td>");
 			html.append("<td>").append(hasPayments ? dict.yes() : "").append("</td>");
+
+			// actions
+			html.append("<td>");
+			html.append("<a href='?page=" + EditBookingPage.NAME + "&amp;id=" + booking.getId() + "'>");
+			html.append(dict.edit());
+			html.append("</a>");
+			if (booking.isRemovable()) {
+				html.append(" <a href='" + removePrefix + booking.getId() + "'>" + dict.remove() + "</a>");
+			}
+			html.append("</td>");
 
 			html.append("</tr>\n");
 		}
